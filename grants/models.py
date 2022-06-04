9ba3396e96e541b8e78d99de47f85188f11aa8c7
Jaesik <joje1283@gmail.com>
2022-06-04 12:40:32 +0900

    Grant, Leave에 대한 다대다 테이블 생성

--- grants/models.py
+++ grants/models.py
@@ -4,3 +4,8 @@ from django.db import models
 class Grant(models.Model):
     member = models.ForeignKey(verbose_name="휴가 대상자", to="members.Member", on_delete=models.CASCADE)
     stock = models.IntegerField(verbose_name="부여된 휴가 일 수")
+
+
+class GrantLeave(models.Model):
+    grant = models.ForeignKey(to="grants.Grant", on_delete=models.DO_NOTHING)
+    leave = models.ForeignKey(to="leaves.Leave", on_delete=models.DO_NOTHING)
