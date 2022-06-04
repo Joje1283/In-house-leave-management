565cc88b25db13172392d646ab12a7b7beb250bd
Jaesik <joje1283@gmail.com>
2022-06-04 13:29:40 +0900

    필드 타입 변경

--- grants/models.py
+++ grants/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Grant(models.Model):
     member = models.ForeignKey(verbose_name="휴가 대상자", to="members.Member", on_delete=models.CASCADE)
-    stock = models.IntegerField(verbose_name="부여된 휴가 일 수")
+    stock = models.FloatField(verbose_name="부여된 휴가 일 수")
 
 
 class GrantLeave(models.Model):
--- leaves/models.py
+++ leaves/models.py
@@ -3,4 +3,4 @@ from django.db import models
 
 class Leave(models.Model):
     name = models.CharField(verbose_name="휴가 종류", max_length=30)
-    consume = models.IntegerField(verbose_name="소진일")
+    consume = models.FloatField(verbose_name="소진일")
