5f9010ab928ea5f6af4333ef25cc23bed50ff62d
Jaesik <joje1283@gmail.com>
2022-06-04 13:17:29 +0900

    필드 이름 변경

--- signs/models.py
+++ signs/models.py
@@ -4,7 +4,7 @@ from django.conf import settings
 
 class Sign(models.Model):
     approver = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING)
-    order = models.OneToOneField(to="grants.GrantLeave", on_delete=models.DO_NOTHING)
+    grant_leave_id = models.OneToOneField(to="grants.GrantLeave", on_delete=models.DO_NOTHING)
 
     class SignType(models.TextChoices):
         CONFIRM = "CONFIRM"
