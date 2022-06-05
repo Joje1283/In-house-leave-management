8c5356a083fdcd271ab4f2b2bf0ea9fcf4618d12
Jaesik <joje1283@gmail.com>
2022-06-05 15:25:58 +0900

    휴가 부여 및 남은 휴가 확인

--- grants/models.py
+++ grants/models.py
@@ -1,9 +1,27 @@
 from django.db import models
+from django.db.models import Sum
+from django.contrib.auth import get_user_model
+
+
+class GrantManager(models.Manager):
+    def grant(self, username, count):
+        member = get_user_model().objects.get_or_validate_not_found_member(username)
+        grant = self.model(member=member, stock=count)
+        grant.save()
+
+    def granted_stock(self, username):
+        data = self.filter(member__username=username).aggregate(Sum("stock"))
+        result = data.get("stock__sum")
+        if result is None:
+            result = 0
+        return result
 
 
 class Grant(models.Model):
     member = models.ForeignKey(verbose_name="휴가 대상자", to="members.Member", on_delete=models.CASCADE)
     stock = models.FloatField(verbose_name="부여된 휴가 일 수")
+
+    objects = GrantManager()
 
     def __str__(self):
         return f"{self.member}에게 {self.stock}일 부여"
