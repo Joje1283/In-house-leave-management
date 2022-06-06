9cbfd3b18052319b2c6997af71ae483f859861f1
Jaesik <joje1283@gmail.com>
2022-06-06 20:34:19 +0900

    휴가 종류 관리 API에서 인증 여부 확인하도록 수정

--- leaves/views.py
+++ leaves/views.py
@@ -1,19 +1,20 @@
-from django.views.generic import CreateView, ListView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import CreateView, ListView, UpdateView
 
 from .models import Leave
 
 
-class LeaveCreateView(CreateView):
+class LeaveCreateView(LoginRequiredMixin, CreateView):
     model = Leave
     success_url = "/"
     fields = "__all__"
 
 
-class LeaveListView(ListView):
+class LeaveListView(LoginRequiredMixin, ListView):
     queryset = Leave.objects.order_by("-pk")
 
 
-class LeaveUpdateView(UpdateView):
+class LeaveUpdateView(LoginRequiredMixin, UpdateView):
     model = Leave
     template_name = "leaves/leave_update_form.html"
     fields = ["consume"]
