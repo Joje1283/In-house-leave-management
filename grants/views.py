5c9f2e3b951f3b05c1458266e3347185a72b30da
Jaesik <joje1283@gmail.com>
2022-06-11 10:08:31 +0900

    휴가 부여 시 인증이 필요하도록 수정

--- grants/views.py
+++ grants/views.py
@@ -1,18 +1,19 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, CreateView, DeleteView
 
 from .models import Grant
 
 
-class GrantListView(ListView):
+class GrantListView(LoginRequiredMixin, ListView):
     queryset = Grant.objects.order_by("-pk")
 
 
-class GrantCreateView(CreateView):
+class GrantCreateView(LoginRequiredMixin, CreateView):
     model = Grant
     fields = ["member", "stock"]
     success_url = "/grants"
 
 
-class GrantDeleteView(DeleteView):
+class GrantDeleteView(LoginRequiredMixin, DeleteView):
     model = Grant
     success_url = "/grants"
