7a339c8db54723cc42f5ddf0403116d3481bd2e8
Jaesik <joje1283@gmail.com>
2022-06-12 22:24:25 +0900

    피플팀 그룹 추가 피플팀 그룹에 멤버, 부여, 휴가타입에 대한 CRUD 권한 부여 피플팀 그룹일 때만 Member, Grant, Leave의 View에 접근 가능하도록 수정

--- grants/views.py
+++ grants/views.py
@@ -1,24 +1,42 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.contrib.auth import logout as auth_logout
 from django.http import HttpResponseRedirect
 from django.views.generic import ListView, CreateView, DeleteView
+from django.contrib import messages
 
 from .exceptions import RemoveGrantException
 from .models import Grant
 
 
-class GrantListView(LoginRequiredMixin, ListView):
+class GrantListView(PermissionRequiredMixin, ListView):
     queryset = Grant.objects.order_by("-pk")
+    permission_required = "grants.view_grant"
+    permission_denied_message = "people팀 계정으로 로그인 바랍니다."
+
+    def handle_no_permission(self):
+        auth_logout(self.request)
+        messages.info(self.request, self.permission_denied_message, "danger")
+        return super(GrantListView, self).handle_no_permission()
 
 
-class GrantCreateView(LoginRequiredMixin, CreateView):
+class GrantCreateView(PermissionRequiredMixin, CreateView):
     model = Grant
     fields = ["member", "stock", "description"]
     success_url = "/grants"
+    permission_required = "grants.add_grant"
+    permission_denied_message = "people팀 계정으로 로그인 바랍니다."
+
+    def handle_no_permission(self):
+        auth_logout(self.request)
+        messages.info(self.request, self.permission_denied_message, "danger")
+        return super(GrantCreateView, self).handle_no_permission()
 
 
-class GrantDeleteView(LoginRequiredMixin, DeleteView):
+class GrantDeleteView(PermissionRequiredMixin, DeleteView):
     model = Grant
     success_url = "/grants"
+    permission_required = "grants.change_grant"
+    permission_denied_message = "people팀 계정으로 로그인 바랍니다."
 
     def form_valid(self, form):
         try:
@@ -28,3 +46,8 @@ class GrantDeleteView(LoginRequiredMixin, DeleteView):
             form.add_error(None, e)
             return self.form_invalid(form)
         return HttpResponseRedirect("/grants")
+
+    def handle_no_permission(self):
+        auth_logout(self.request)
+        messages.info(self.request, self.permission_denied_message, "danger")
+        return super(GrantDeleteView, self).handle_no_permission()
--- members/views.py
+++ members/views.py
@@ -1,5 +1,6 @@
 from django.contrib import messages
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.contrib.auth import logout as auth_logout
 from django.http import HttpResponseRedirect
 from django.views.generic import CreateView, ListView, UpdateView
 
@@ -25,9 +26,11 @@ def logout(request):
     return logout_then_login(request)
 
 
-class MemberCreateView(LoginRequiredMixin, CreateView):
+class MemberCreateView(PermissionRequiredMixin, CreateView):
     model = Member
     form_class = MemberForm
+    permission_required = ("members.add_member",)
+    permission_denied_message = "people팀 계정으로 로그인 바랍니다."
 
     def form_valid(self, form):
         try:
@@ -37,19 +40,38 @@ class MemberCreateView(LoginRequiredMixin, CreateView):
             return self.form_invalid(form)
         return HttpResponseRedirect("/")
 
+    def handle_no_permission(self):
+        auth_logout(self.request)
+        messages.info(self.request, self.permission_denied_message, "danger")
+        return super(MemberCreateView, self).handle_no_permission()
 
-class MemberListView(LoginRequiredMixin, ListView):
+
+class MemberListView(PermissionRequiredMixin, ListView):
     queryset = Member.objects.order_by("-pk")
+    permission_required = "members.view_member"
+    permission_denied_message = "people팀 계정으로 로그인 바랍니다."
+
+    def handle_no_permission(self):
+        auth_logout(self.request)
+        messages.info(self.request, self.permission_denied_message, "danger")
+        return super(MemberListView, self).handle_no_permission()
 
 
-class MemberUpdateView(LoginRequiredMixin, UpdateView):
+class MemberUpdateView(PermissionRequiredMixin, UpdateView):
     model = Member
     form_class = MemberUpdateForm
     slug_field = "username"
     template_name = "members/member_update_form.html"
+    permission_required = "members.change_member"
+    permission_denied_message = "people팀 계정으로 로그인 바랍니다."
 
     def form_valid(self, form):
         username = self.kwargs.get("slug")
         approver_name = form.cleaned_data.get("approver").username
         self.model.objects.update_approver(username, approver_name)
         return HttpResponseRedirect("/members")
+
+    def handle_no_permission(self):
+        auth_logout(self.request)
+        messages.info(self.request, self.permission_denied_message, "danger")
+        return super(MemberUpdateView, self).handle_no_permission()
