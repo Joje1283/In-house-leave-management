eab59b9218f6d3e9937b2ec682eb64cc7bd24883
Jaesik <joje1283@gmail.com>
2022-06-19 17:44:14 +0900

    shell에서 task 실행방법 주석 추가

--- push/tasks.py
+++ push/tasks.py
@@ -1,5 +1,14 @@
 from celery import shared_task
 
+"""
+# 실행 방법 예시 (shell)
+
+from datetime import datetime, timedelta
+from push.tasks import push_print
+
+push_print.apply_async(["안녕하세요 폴222"], eta=datetime.now() + timedelta(seconds=10), queue="paul_worker")
+"""
+
 
 @shared_task
 def push_print(s):
