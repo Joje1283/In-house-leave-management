6930569c9058441d964379dda1a46c914f8719fe
Jaesik <joje1283@gmail.com>
2022-07-23 19:17:44 +0900

    allowed host 를 입력받도록 수정

--- leave_management/settings/prod.py
+++ leave_management/settings/prod.py
@@ -6,7 +6,8 @@ from sentry_sdk.integrations.django import DjangoIntegration
 
 # Nginx가 프록시 서버 역할을 하고있고, http://127.0.0.1:8000으로 리디렉션 하고있기에,
 # 허용 API를 127.0.0.1로 설정함.
-ALLOWED_HOSTS = ["127.0.0.1", "192.168.49.2"]
+ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS += [os.getenv("ALLOWED_HOST")]
 CSRF_TRUSTED_ORIGINS = ["https://*.joje.link"]  # 안전하지 않은 요청에 대한 신뢰할 수 있는 출처 목록 (CSRF 오류로 인해 설정)
 DEBUG = False
 
