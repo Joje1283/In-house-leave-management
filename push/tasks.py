9af57a2ec04108d1aaff7f7652a028958fd3d015
Jaesik <joje1283@gmail.com>
2022-07-23 23:10:57 +0900

    로그 추가

--- push/tasks.py
+++ push/tasks.py
@@ -23,7 +23,9 @@ def push_print(s):
 @shared_task
 def send_welcome_email(username: str):
     try:
+        print("start send_welcome_email")
         member = Member.objects.get(username=username)
+        print(f"{member.username}에게 메일을 보냅니다.")
         result = member.send_welcome_email()
         print(result)
     except Exception as e:
